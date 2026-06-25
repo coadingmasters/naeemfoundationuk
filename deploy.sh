f45f07891d3b7506529cf375e48f0815406088ca
Ahsan Nawaz <coadingmasters@gmail.com>
2026-06-25 09:00:59 +0000

    Update 2026-06-25 09:00

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ git add .
 git commit -m "Update $(date '+%Y-%m-%d %H:%M')"
 git push origin main
 echo "Deploying to server..."
-scp -P 65002 -r public/build u721035936@145.79.26.12:/home/u721035936/domains/naeemfoundation.qubexistech.com/public_html/
+scp -P 65002 -r public/build public/images u721035936@145.79.26.12:/home/u721035936/domains/naeemfoundation.qubexistech.com/public_html/
 ssh -p 65002 u721035936@145.79.26.12 "cd ~/domains/naeemfoundation.qubexistech.com/naeemfoundation && git pull origin main && php artisan view:clear && php artisan cache:clear"
 echo "Done! Site is live."
